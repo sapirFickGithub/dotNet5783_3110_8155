4ad919764d1c9f59035d31c208824cfc5bc5c938
sapirFickGithub <fick@g.jct.ac.il>
2022-10-27 13:43:37 +0300

    initial code + Welcom functions

---
+++ Stage0/Program8155.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Stage0
+{
+    partial class Porogram
+    {
+        static void Main(string[] args)
+        {
+            Welcome8155();
+            Welcome3110();
+
+
+        }
+        static partial void Welcome3110();
+
+        private static void Welcome8155()
+        {
+            Console.WriteLine("enter name");
+            string name = Console.ReadLine();
+            Console.WriteLine("tnx for ");
+            Console.ReadKey();
+        }
+    }
+}
