56dece723132fae9087b6607e09c42d486d26db7
sapirFickGithub <fick@g.jct.ac.il>
2022-11-29 17:31:59 +0200

    BL file

---
+++ BL/BlImplementation/BL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+
+namespace BlImplementation
+{
+    sealed public class BL : IBl
+    {
+        public IProduct Product => new Product();
+        public IOrder Order => new Order();
+        public ICart Cart => new Cart();
+
+    }
+}
