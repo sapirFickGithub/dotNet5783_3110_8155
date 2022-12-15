b0f6eb9ea22bbbc05dc171657fb00acabd30ad51
sapirFickGithub <fick@g.jct.ac.il>
2022-12-15 16:45:36 +0200

    internal

--- BL/BlImplementation/BL.cs
+++ BL/BlImplementation/BL.cs
@@ -5,13 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using BlApi;
 
-namespace BlImplementation
-{
-    sealed public class BL : IBl
-    {
-        public IProduct Product => new Product();
-        public IOrder Order => new Order();
-        public ICart Cart => new Cart();
+namespace BlImplementation;
 
-    }
+sealed internal class BL : IBl
+{
+    public IProduct Product => new Product();
+    public IOrder Order => new Order();
+    public ICart Cart => new Cart();
+
 }
