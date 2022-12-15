166e5958cdb30db4855dcfa67e880f1e2184d61d
Hadar <hadarsarusi21@gmail.com>
2022-12-15 10:48:04 +0200

    convert dallidt to singelton

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -7,8 +7,10 @@ using DO;
 using DalApi;
 namespace Dal
 {
-    sealed public class DalList : IDal
+    sealed internal class DalList : IDal
     {
+        public static IDal Instance { get; } = new DalList();
+        private DalList() { }//constractor 
         public IProduct Product => new DalProduct();
         public IOrder Order => new DalOrder();
         public IOrderItem OrderItem => new DalOrderItem();
