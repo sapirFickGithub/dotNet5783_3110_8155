0d81820ad3af1f96e8aa451c69bfca4268f66837
sapirFickGithub <fick@g.jct.ac.il>
2023-01-05 16:34:16 +0200

    tracking order- don!

--- PL/CartWindows/CartNew.xaml.cs
+++ PL/CartWindows/CartNew.xaml.cs
@@ -22,96 +22,12 @@ namespace PL.CartWindows
     public partial class CartNew : Window
     {
         private static BlApi.IBl? bl = BlApi.Factory.Get();
-        public bool hasSorted = true;
+      
         public CartNew()
         {
             InitializeComponent();
-            List_of_product.ItemsSource = bl.Product.getListOfProductItem();
+            
         }
 
-        private void BackToMainWindow_Click(object sender, RoutedEventArgs e)
-        {
-            //move to Main window
-            new MainWindow().Show();
-            this.Close();
-        }
-
-        private void Sort_By_Name_Click(object sender, RoutedEventArgs e)
-        {
-            var listTemp = bl.Product.getListOfProduct();
-            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(List_of_product.ItemsSource);
-            view.SortDescriptions.Clear();
-            if (hasSorted)
-            {
-
-                view.SortDescriptions.Add(new("Name", ListSortDirection.Descending));
-                hasSorted = false;
-            }
-            else
-            {
-                view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
-                hasSorted = true;
-            }
-        }
-
-        private void Sort_By_ID_Click(object sender, RoutedEventArgs e)
-        {
-            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(List_of_product.ItemsSource);
-            view.SortDescriptions.Clear();
-            if (hasSorted)
-            {
-                view.SortDescriptions.Add(new SortDescription("ID", ListSortDirection.Descending));
-                hasSorted = false;
-            }
-            else
-            {
-                view.SortDescriptions.Add(new SortDescription("ID", ListSortDirection.Ascending));
-                hasSorted = true;
-            }
-        }
-
-        private void Sort_By_Price_Click(object sender, RoutedEventArgs e)
-        {
-            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(List_of_product.ItemsSource);
-            view.SortDescriptions.Clear();
-            if (hasSorted)
-            {
-                view.SortDescriptions.Add(new SortDescription("Price", ListSortDirection.Descending));
-                hasSorted = false;
-            }
-            else
-            {
-                view.SortDescriptions.Add(new SortDescription("Price", ListSortDirection.Ascending));
-                hasSorted = true;
-            }
-        }
-
-        private void Category_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            try
-            {
-                Object selectedItem = Category_selector.SelectedItem;
-                if (selectedItem.ToString() == "All")
-                    List_of_product.ItemsSource = bl.Product.getListOfProductItem();
-                else
-                    List_of_product.ItemsSource = bl.Product.getListOfProductItem(a => a?.ProductCategory.ToString() == selectedItem.ToString());
-            }
-            catch (Exception)
-            {
-                MessageBox.Show(
-                         "Somthing went worng...\n please try again later",
-                         "Unknown error",
-                         // MessageBoxButton.OKCancel,
-                         MessageBoxButton.OK,
-                         MessageBoxImage.Hand,
-                         MessageBoxResult.Cancel,
-                         MessageBoxOptions.RtlReading);
-            }
-        }
-
-        private void List_of_product_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
     }
 }
--- PL/OrderWindows/Tracking.xaml.cs
+++ PL/OrderWindows/Tracking.xaml.cs
@@ -20,57 +20,26 @@ using System.Collections.ObjectModel;
 /// </summary>
 namespace PL.OrderWindows
 {
-    /// <summary>
-    /// Interaction logic for Tracking.xaml
-    /// </summary>
 
-    public partial class Tracking : INotifyPropertyChanged
+
+    public partial class Tracking : Window
     {
         private static BlApi.IBl? bl = BlApi.Factory.Get();
 
-   public Tracking()
+ public BO.OrderTracking orderTrack { set; get; }
+
+        public Tracking(int id)
         {
+            orderTrack = bl?.Order.orderTracking(id);
             InitializeComponent();
         }
-        private string _status;
-        public string Status
-        {
-            get { return _status; }
-            set
-            {
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs(_status));
-
-            }
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        public BO.OrderTracking orderTrack { set; get; }
-
-     
-
-        private void OnPropertyChanged(string track)
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(track));
-        }
+        
         private void BackToMain_Click(object sender, RoutedEventArgs e)
         {
             new MainWindow().Show();
             this.Close();
         }
 
-        private void GoTracking_Click(object sender, RoutedEventArgs e)
-        {
-            int.TryParse(ID.Text, out int id);
-            orderTrack = bl?.Order.orderTracking(id);
-            _status = orderTrack.Status.ToString();
-        }
-
-        private void ID_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
 
     }
 }
